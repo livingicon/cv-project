a547d9613b94d700deb33469794c1af1e1b7f979
Josh Hancock <livingicon@gmail.com>
2023-01-03 16:56:02 -0500

    Add CVPreview component and link to App

--- src/App.js
+++ src/App.js
@@ -3,20 +3,39 @@
 import GeneralInfo from "./components/GeneralInfo";
 import EducationalExp from "./components/EducationalExp";
 import PracticalExp from "./components/PracticalExp";
+import CVPreview from "./components/CVPreview";
 import React, { Component } from "react";
  
 class App extends Component {
- render() {
-   return (
-    <div>
-      <h1>CV Creator</h1>
-      <GeneralInfo></GeneralInfo>
-      <EducationalExp></EducationalExp>
-      <PracticalExp></PracticalExp>
-    </div>
 
-   );
- }
+  render() {
+    return (
+      <div>
+
+        <div className="header">
+          <h1>CV Creator</h1>
+        </div>
+
+        <div className="main">
+          <div className="cvForms">
+            <GeneralInfo></GeneralInfo>
+            <EducationalExp></EducationalExp>
+            <PracticalExp></PracticalExp>
+          </div>
+
+          <div className="cvPreview">
+            <CVPreview></CVPreview>
+          </div>
+
+        </div>
+
+        <div className="footer">
+          <p>Copyright &copy; 2023 Joshua Hancock</p>
+        </div>
+
+      </div>
+    );
+  }
 }
  
 export default App;
---
+++ src/components/CVPreview.js
@@ -0,0 +1,15 @@
+// CVPreview.js
+ 
+import React, { Component } from 'react';
+ 
+class CVPreview extends Component {
+  render() {
+    return (
+      <div className="cv_preview">
+        <h1>CV Preview</h1>
+      </div>
+    );
+  }
+}
+ 
+export default CVPreview;
