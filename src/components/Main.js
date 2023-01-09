5e7259982db22f6a192e3478668bb77c2396f976
Josh Hancock <livingicon@gmail.com>
2023-01-09 18:44:34 -0500

    Fix import statements in Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,36 @@
 // Main.js
  
 import React, { Component } from 'react';
-import GeneralInfo from "./GeneralInfo";
-import EducationalExp from "./EducationalExp";
-import PracticalExp from "./PracticalExp";
-import CVPreview from "./CVPreview";
+import GeneralInfo from "./Main/GeneralInfo";
+import EducationalExp from "./Main/EducationalExp";
+import PracticalExp from "./Main/PracticalExp";
+import CVPreview from "./Main/CVPreview";
  
 class Main extends Component {
+  // constructor() {
+  //   super();
+
+  //   this.state = {
+  //     task: {
+  //       text: '', 
+  //       id: uniqid()
+  //     },
+  //     tasks: [],
+  //   };
+  // }
+
+  // handleChange = (e) => {
+  //   this.setState({
+  //     task: {
+  //       text: e.target.value,
+  //       id: this.state.task.id,
+  //     },
+  //   });
+  // };
+
   render() {
+    // const {} = this.state;
+
     return (
       <div className="main">
         <GeneralInfo />
