c94994971031e679713fe9a41d4395158ff2d571
Josh Hancock <livingicon@gmail.com>
2023-01-14 15:40:46 -0500

    Add EdForm.js and move the ed form there

---
+++ src/components/Main/EdForm.js
@@ -0,0 +1,61 @@
+// EdForm.js
+
+import React, { Component } from 'react';
+ 
+class EdForm extends Component {
+
+  render() {
+    const { edCount, handleChangeInput, school, degree, degreeFrom, degreeTo } = this.props;
+
+    return (
+<div className="forms">
+        <form id="education">
+          <div>
+            <label htmlFor="school">organization administering degree or certification</label>
+            <input 
+              onChange={handleChangeInput}
+              value={school}
+              type="text" 
+              id="school" 
+              name={`school${edCount}`}
+              placeholder="Purdue University"
+              autoComplete="off"
+            />
+          </div>
+          <div>
+            <label htmlFor="degree">degree or certification</label>
+            <input
+              onChange={handleChangeInput}
+              value={degree}
+              type="text" 
+              id="degree" 
+              name={`degree${edCount}`} 
+              placeholder="Bachelor of Arts in Computer Science" 
+              autoComplete="off"
+            />
+          </div>
+          <div>
+            <label htmlFor="degreeFrom">from</label>
+            <input 
+              onChange={handleChangeInput}
+              value={degreeFrom}
+              type="date" 
+              id="degreeFrom" 
+              name={`degreeFrom${edCount}`}
+            />
+            <label htmlFor="degreeTo">to</label>
+            <input 
+              onChange={handleChangeInput}
+              value={degreeTo}
+              type="date" 
+              id="degreeTo" 
+              name={`degreeTo${edCount}`}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default EdForm;
--- src/components/Main/EducationalExp.js
+++ src/components/Main/EducationalExp.js
@@ -4,69 +4,14 @@ import React, { Component } from 'react';
  
 class EducationalExp extends Component {
 
-  duplicateForm = () => {
-    // console.log(this.state.edCount);
-    for(let i=0; i<this.state.edCount; i++) {
-      console.log("add form");
-    }
-  }
-
   render() {
-    const { edCount, handleChangeInput, school, degree, degreeFrom, degreeTo, addEd } = this.props;
+    const { setEd, addEd } = this.props;
 
     return (
-      
-
-      <div className="forms">
-        <form id="education">
-          <div>
-            <label htmlFor="school">organization administering degree or certification</label>
-            <input 
-              onChange={handleChangeInput}
-              value={school}
-              type="text" 
-              id="school" 
-              name={`school${edCount}`}
-              placeholder="Purdue University"
-              autoComplete="off"
-            />
-          </div>
-          <div>
-            <label htmlFor="degree">degree or certification</label>
-            <input
-              onChange={handleChangeInput}
-              value={degree}
-              type="text" 
-              id="degree" 
-              name={`degree${edCount}`} 
-              placeholder="Bachelor of Arts in Computer Science" 
-              autoComplete="off"
-            />
-          </div>
-          <div>
-            <label htmlFor="degreeFrom">from</label>
-            <input 
-              onChange={handleChangeInput}
-              value={degreeFrom}
-              type="date" 
-              id="degreeFrom" 
-              name={`degreeFrom${edCount}`}
-            />
-            <label htmlFor="degreeTo">to</label>
-            <input 
-              onChange={handleChangeInput}
-              value={degreeTo}
-              type="date" 
-              id="degreeTo" 
-              name={`degreeTo${edCount}`}
-            />
-          </div>
-          {/* <button 
-            onClick={addEd}
-            className="btns" 
-            type="submit"
-            >add education</button> */}
-        </form>
+      <div>
+        <h2>Educational Experience</h2>
+        {setEd}
+        <button onClick={addEd}>add education</button>
       </div>
     );
   }
