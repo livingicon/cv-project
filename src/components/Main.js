e98adf929678b09fe36b1e60afa8bbedd58918aa
Josh Hancock <livingicon@gmail.com>
2023-01-25 15:08:14 -0500

    Transition more of Main.js to function component

--- src/components/Main.js
+++ src/components/Main.js
@@ -36,16 +36,134 @@ const Main = (props) => {
     />
   ]);
 
+  const handleChangeInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmitPreviewCV = (e) => { 
+    const general = document.getElementById('general');
+    const edForms = document.getElementsByClassName('edForms');
+    const praxForms = document.getElementsByClassName('praxForms');
+    const add = document.getElementsByClassName('add');
+    const generalCV = document.getElementById('generalCV');
+    const edCV = document.getElementsByClassName('edCV');
+    const praxCV = document.getElementsByClassName('praxCV');
+    if (e.target.innerHTML === 'Preview CV') {
+      e.target.innerHTML = 'Edit CV'
+      // hide
+      general.style.display = 'none';
+      for (let i=0; i<edForms.length; i++){
+        edForms[i].style.display = 'none';
+      }
+      for (let i=0; i<praxForms.length; i++){
+        praxForms[i].style.display = 'none';
+      }
+      for (let i=0; i<add.length; i++){
+        add[i].style.display = 'none';
+      }
+      // unhide
+      generalCV.style.display = 'flex';
+      for (let i=0; i<edCV.length; i++){
+        edCV[i].style.display = 'flex';
+      }
+      for (let i=0; i<praxCV.length; i++){
+        praxCV[i].style.display = 'flex';
+      }
+
+      const orderedSchool = {};
+      this.state.setEd.map(obj => {
+        orderedSchool[`school${obj.key}`] = this.state[`school${obj.key}`];
+        orderedSchool[`degree${obj.key}`] = this.state[`degree${obj.key}`];
+        orderedSchool[`degreeFrom${obj.key}`] = this.state[`degreeFrom${obj.key}`];
+        orderedSchool[`degreeTo${obj.key}`] = this.state[`degreeTo${obj.key}`];
+      })
+      const orderedPrax = {};
+      this.state.setPrax.map(obj => {
+        orderedPrax[`position${obj.key}`] = this.state[`position${obj.key}`];
+        orderedPrax[`employer${obj.key}`] = this.state[`employer${obj.key}`];
+        orderedPrax[`tasks${obj.key}`] = this.state[`tasks${obj.key}`];
+        orderedPrax[`jobFrom${obj.key}`] = this.state[`jobFrom${obj.key}`];
+        orderedPrax[`jobTo${obj.key}`] = this.state[`jobTo${obj.key}`];
+      })
+      this.setState({
+        edList: {...this.state.edList, ...orderedSchool},
+        praxList: {...this.state.praxList, ...orderedPrax},
+      })
+    } else if (e.target.innerHTML === 'Edit CV') {
+      e.target.innerHTML = 'Preview CV';
+      // unhide
+      general.style.display = 'block';
+      for (let i=0; i<edForms.length; i++){
+        edForms[i].style.display = 'block';
+      }
+      for (let i=0; i<praxForms.length; i++){
+        praxForms[i].style.display = 'block';
+      }
+      for (let i=0; i<add.length; i++){
+        add[i].style.display = 'block';
+      }
+      // hide
+      generalCV.style.display = 'none';
+      for (let i=0; i<edCV.length; i++){
+        edCV[i].style.display = 'none';
+      }
+      for (let i=0; i<praxCV.length; i++){
+        praxCV[i].style.display = 'none';
+      }
+    }
+  }
+
+  const addForm = (e) => {
+    if(e.target.innerHTML === "add education") {
+      this.setState({
+        edCount: this.state.edCount + 1,
+        setEd: this.state.setEd.concat(
+          <EdForm 
+            edCount={this.state.edCount + 1} 
+            key={this.state.edCount + 1}
+            handleChangeInput={this.handleChangeInput}
+            deleteForm={this.deleteForm} />
+        )
+      });
+    } else {
+      this.setState({
+        praxCount: this.state.praxCount + 1,
+        setPrax: this.state.setPrax.concat(
+          <PraxForm 
+            praxCount={this.state.praxCount + 1} 
+            key={this.state.praxCount + 1}
+            handleChangeInput={this.handleChangeInput}
+            deleteForm={this.deleteForm} />
+        )
+      });
+    }
+  }
+
+  const deleteForm = (e) => {
+    e.preventDefault();
+    if(e.target.innerHTML === "remove education"){
+      this.setState({
+        setEd: this.state.setEd.filter(form => e.target.dataset.btn !== form.key),
+      });
+    } else {
+      this.setState({
+        setPrax: this.state.setPrax.filter(form => e.target.dataset.btn !== form.key),
+      });
+    }
+  }
+
   return (
     <div className="main">
       <CVPreview 
-        onSubmitPreviewCV={this.onSubmitPreviewCV}
+        onSubmitPreviewCV={onSubmitPreviewCV}
       />
 
       <h2>General Information</h2>
 
       {<GeneralInfo
-        handleChangeInput={this.handleChangeInput} />} 
+        handleChangeInput={handleChangeInput} />} 
       {<GeneralCV 
         firstName={this.state.firstName}
         lastName={this.state.lastName}
@@ -56,10 +174,10 @@ const Main = (props) => {
       <h2>Educational Experience</h2>
 
       {<EducationalExp 
-        handleChangeInput={this.handleChangeInput}
+        handleChangeInput={handleChangeInput}
         setEd={this.state.setEd}
-        addForm={this.addForm}
-        deleteForm={this.deleteForm} /> }
+        addForm={addForm}
+        deleteForm={deleteForm} /> }
       {<EducationCV 
         setEd={this.state.setEd}
         edList={this.state.edList}
@@ -68,10 +186,10 @@ const Main = (props) => {
 
       <h2>Practical Experience</h2>
       {<PracticalExp 
-        handleChangeInput={this.handleChangeInput}
+        handleChangeInput={handleChangeInput}
         setPrax={this.state.setPrax}
-        addForm={this.addForm}
-        deleteForm={this.deleteForm} /> }
+        addForm={addForm}
+        deleteForm={deleteForm} /> }
       {<PracticalCV 
         setPrax={this.state.setPrax}
         praxList={this.state.praxList}
